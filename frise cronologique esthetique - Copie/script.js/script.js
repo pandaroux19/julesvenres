f82c77fc30cd445174c1c55bcbd7f474072b9cee
pandaroux19 <146937290+pandaroux19@users.noreply.github.com>
2023-10-25 19:02:52 +0200

    Add files via upload

---
+++ frise cronologique esthetique - Copie/script.js/script.js
@@ -0,0 +1,93 @@
+
+//image 1
+
+let c1 = document.getElementById('info1').onclick = function() {
+  let inf1 = document.querySelector('.image1').classList.toggle("hidden")
+}
+
+let c1c = document.getElementById('croix1').onclick = function() {
+  let inf1 = document.querySelector('.image1').classList.toggle("hidden")
+}
+
+
+
+// image 2
+
+
+
+let c2 = document.getElementById('info2').onclick = function() {
+  let inf2 = document.querySelector('.image2').classList.toggle("hidden")
+}
+
+let c2c = document.getElementById('croix2').onclick = function() {
+  let inf2 = document.querySelector('.image2').classList.toggle("hidden")
+}
+
+//image3
+
+
+
+let c3 = document.getElementById('info3').onclick = function() {
+  let inf3 = document.querySelector('.image3').classList.toggle("hidden")
+}
+
+let c3c = document.getElementById('croix3').onclick = function() {
+  let inf3 = document.querySelector('.image3').classList.toggle("hidden")
+}
+
+
+// image4
+
+let c4 = document.getElementById('info4').onclick = function() {
+  let inf4 = document.querySelector('.image4').classList.toggle("hidden")
+}
+
+let c4c = document.getElementById('croix4').onclick = function() {
+  let inf4 = document.querySelector('.image4').classList.toggle("hidden")
+}
+
+// image5
+
+
+let c5 = document.getElementById('info5').onclick = function() {
+  let inf5 = document.querySelector('.image5').classList.toggle("hidden")
+}
+
+let c5c = document.getElementById('croix5').onclick = function() {
+  let inf5 = document.querySelector('.image5').classList.toggle("hidden")
+}
+
+
+// image6
+
+
+let c6 = document.getElementById('info6').onclick = function() {
+  let inf6 = document.querySelector('.image6').classList.toggle("hidden")
+}
+
+let c6c = document.getElementById('croix6').onclick = function() {
+  let inf6 = document.querySelector('.image6').classList.toggle("hidden")
+}
+
+// image7
+
+
+let c7 = document.getElementById('info7').onclick = function() {
+  let inf7  = document.querySelector('.image7').classList.toggle("hidden")
+}
+
+let c7c = document.getElementById('croix7').onclick = function() {
+  let inf7 = document.querySelector('.image7').classList.toggle("hidden")
+}
+
+// image8 
+
+
+let c8 = document.getElementById('info8').onclick = function() {
+  let inf8 = document.querySelector('.image8').classList.toggle("hidden")
+}
+
+let c8c = document.getElementById('croix8').onclick = function() {
+  let inf8 = document.querySelector('.image8').classList.toggle("hidden")
+}
+
